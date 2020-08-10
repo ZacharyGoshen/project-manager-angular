7fb3484e043a5093b5df51a722c37b36d1b4b57a
Zachary Goshen <zachgoshen@gmail.com>
2020-08-10 08:30:50 -0400

    Add task name validation and create unit test project

--- ProjectManagerAngular/Models/Task.cs
+++ ProjectManagerAngular/Models/Task.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectManagerAngular.Models
 {
     public class Task
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; }
     }
 }
